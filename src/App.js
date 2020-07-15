b7ba2858f74919c5e3ef01039ad64d1b65ac4c21
frederico-campello <fredfmc33@gmail.com>
2020-07-15 19:16:52 -0300

    Continuando

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/settings" component={Settings} />
       </Switch>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,97 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Home extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      redirectConfig: false,
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.renderInputs = this.renderInputs.bind(this);
+    this.renderBtnPlay = this.renderBtnPlay.bind(this);
+    this.renderBtnConfig = this.renderBtnConfig.bind(this);
+    this.handleRedirect = this.handleRedirect.bind(this);
+  }
+
+  // localStorage.setItem('token', json.token)
+
+  handleInputChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleRedirect() {
+    this.setState({
+      redirectConfig: true,
+    });
+  }
+
+  renderInputs() {
     return (
       <div>
-        <div>
-          <label htmlFor="email">
-            Email do Gravatar:
-            <input data-testid="input-gravatar-email" type="email" id="email" />
-          </label>
-          <label htmlFor="username">
-            Nome do Jogador:
-            <input data-testid="input-player-name" type="text" id="username" />
-          </label>
-          <button data-testid="btn-play" type="button">
-            JOGAR!
-          </button>
-        </div>
-        <div>
-          <button data-testid="btn-settings" type="button">
-            Configurações
-          </button>
-        </div>
+        <label htmlFor="email">
+          Email do Gravatar:
+          <input
+            data-testid="input-gravatar-email"
+            type="email"
+            name="email"
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <label htmlFor="username">
+          Nome do Jogador:
+          <input
+            data-testid="input-player-name"
+            type="text"
+            name="username"
+            onChange={this.handleInputChange}
+          />
+        </label>
+      </div>
+    );
+  }
+
+  renderBtnPlay() {
+    const { email, username } = this.state;
+    return (
+      <button
+        data-testid="btn-play"
+        type="button"
+        id="play"
+        disabled={!email || !username}
+      >
+        JOGAR!
+      </button>
+    );
+  }
+
+  renderBtnConfig() {
+    return (
+      <div>
+        <button
+          data-testid="btn-settings"
+          type="button"
+          onClick={this.handleRedirect}
+        >
+          Configurações
+        </button>
+      </div>
+    );
+  }
+
+  render() {
+    const { redirectConfig } = this.state;
+
+    if (redirectConfig) return <Redirect to="/settings" />;
+    return (
+      <div>
+        {this.renderInputs()}
+        {this.renderBtnPlay()}
+        {this.renderBtnConfig()}
       </div>
     );
   }
