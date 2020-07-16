2d369025858717b033cdfdadc5d851a2843b74bc
Leandro Marten <leandromarten1@gmail.com>
2020-07-16 15:34:59 -0300

    added route in app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
+import Feedback from  './pages/Feedback';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/game" component={Game} />
         <Route exact path="/settings" component={Settings} />
+        <Route exact path="/feedback" component={Feedback} />
       </Switch>
     </Router>
   );
