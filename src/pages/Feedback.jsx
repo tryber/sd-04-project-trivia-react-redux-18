00789605d0cc869bd9d07a824c7521e641ac2051
Leandro Marten <leandromarten1@gmail.com>
2020-07-16 15:45:21 -0300

    Add feedback message

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class Feedback extends Component {
+  feedbackMessage(score) {
+    return (score >= 3) ? 'Mandou bem!' : 'Podia ser melhor...';
+  }
+
   render() {
     return (
       <div>
         <h1>Feedback</h1>
+        <h3 data-testid="feedback-text">{this.feedbackMessage()}</h3>
       </div>
     );
   }
